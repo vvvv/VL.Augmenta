d16c9ab0735a82fb0b7e525cd2ec045c6b0a766b
joreg <joreg@vvvv.org>
2025-03-27 18:12:00 +0100

    switching to augmenta nuget

--- src/PObject.cs
+++ src/PObject.cs
@@ -1,170 +1,144 @@
-using System.Numerics;
+using Stride.Core.Mathematics;
 using System.Runtime.CompilerServices;
 
 namespace Augmenta
 {
-    public class PObject
+    public class PObject: GenericObject<Vector3>
     {
-        public int objectID;
-        private Vector3[] pointsA = new Vector3[0];
-        private int pointCount;
-        public ArraySegment<Vector3> points => new ArraySegment<Vector3>(pointsA, 0, pointCount);
+        public PObject()
+            : base() { }
 
-        public Matrix4x4 transform;
-        internal Matrix4x4 parentTransform = Matrix4x4.Identity;
+        //public int objectID;
+        //private Vector3[] pointsA = new Vector3[0];
+        //private int pointCount;
+        //public ArraySegment<Vector3> points => new ArraySegment<Vector3>(pointsA, 0, pointCount);
 
-        //cluster
-        public enum State { Enter = 0, Update = 1, Leave = 2, Ghost = 3 };
-        public State state;
+        //public Matrix4x4 transform;
+        //internal Matrix4x4 parentTransform = Matrix4x4.Identity;
 
-        public Vector3 centroid;
-        public Vector3 velocity;
-        public Vector3 minBounds;
-        public Vector3 maxBounds;
-        //[Range(0, 1)]
-        public float weight;
+        ////cluster
+        //public enum State { Enter = 0, Update = 1, Leave = 2, Ghost = 3 };
+        //public State state;
 
-        float lastUpdateTime;
+        //public Vector3 centroid;
+        //public Vector3 velocity;
+        //public Vector3 minBounds;
+        //public Vector3 maxBounds;
+        ////[Range(0, 1)]
+        //public float weight;
 
-        public float killDelayTime = 0;
+        //float lastUpdateTime;
 
-        public float timeSinceGhost;
-        public bool drawDebug;
+        //public float killDelayTime = 0;
 
-        public enum PositionUpdateMode { None, Centroid, BoxCenter }
-        public PositionUpdateMode posUpdateMode = PositionUpdateMode.Centroid;
+        //public float timeSinceGhost;
+        //public bool drawDebug;
 
-        public enum CoordMode { Absolute, Relative }
-        public CoordMode pointMode = CoordMode.Relative;
+        //public enum PositionUpdateMode { None, Centroid, BoxCenter }
+        //public PositionUpdateMode posUpdateMode = PositionUpdateMode.Centroid;
 
-        public delegate void OnRemoveEvent(PObject obj);
-        public event OnRemoveEvent onRemove;
+        //public enum CoordMode { Absolute, Relative }
+        //public CoordMode pointMode = CoordMode.Relative;
+
+        //public delegate void OnRemoveEvent(PObject obj);
+        //public event OnRemoveEvent onRemove;
 
 
-        // Update is called once per frame
-        internal void Update(float time)
-        {
-            if (time - lastUpdateTime > .5f) 
-                timeSinceGhost = time;
-            else 
-                timeSinceGhost = -1;
-        }
-
-        public void updateData(float time, ReadOnlySpan<byte> data, int offset)
-        {
-            var pos = offset + 1 + 2 * sizeof(int); //packet type (1) + packet size (4) + objectID (4)
-            while (pos < data.Length)
-            {
-                var propertyID = Utils.ReadInt(data, pos);
-                var propertySize = Utils.ReadInt(data, pos + sizeof(int));
-
-                if (propertySize < 0)
-                {
-                    //Debug.LogWarning("Error : property size < 0");
-                    break;
-                }
-
-                switch (propertyID)
-                {
-                    case 0: updatePointsData(data, pos + 2 * sizeof(int)); break;
-                    case 1: updateClusterData(data, pos + 2 * sizeof(int)); break;
-                }
-
-                pos += propertySize;
-            }
-
-            lastUpdateTime = time;
-        }
-
-        void updatePointsData(ReadOnlySpan<byte> data, int offset)
-        {
-            pointCount = Utils.ReadInt(data, offset);
-            var vectors = Utils.ReadVectors(data, offset + sizeof(int), pointCount * Unsafe.SizeOf<Vector3>());
-
-            if (pointsA.Length < pointCount)
-                pointsA = new Vector3[(int)(pointCount * 1.5)];
-
-            if (pointMode == CoordMode.Absolute)
-                vectors.CopyTo(pointsA.AsSpan());
-            else
-            {
-                for (int i = 0; i < vectors.Length; i++)
-                    pointsA[i] = Vector3.Transform(vectors[i], parentTransform);
-            }
-        }
-
-        void updateClusterData(ReadOnlySpan<byte> data, int offset)
-        {
-            state = (State)Utils.ReadInt(data, offset);
-            if (state == State.Leave) //Will leave
-            {
-                onRemove(this);
-                return;
-            }
-
-            var clusterData = new Vector3[4];
-            for (int i = 0; i < 4; i++)
-            {
-                var si = offset + sizeof(int) + i * Unsafe.SizeOf<Vector3>();
-
-                var p = Utils.ReadVector(data, si);
-                if (pointMode == CoordMode.Absolute || i == 1) //i=1 -> don't transform the velocity 
-                    clusterData[i] = p; 
-                else
-                    clusterData[i] = Vector3.Transform(p, parentTransform);//  parentTransform.InverseTransformPoint(p);
-            }
-
-            centroid = clusterData[0];
-            velocity = clusterData[1];
-            minBounds = clusterData[2];
-            maxBounds = clusterData[3];
-            weight = Utils.ReadFloat(data, offset + 4 + 4 * Unsafe.SizeOf<Vector3>());
-
-            switch (posUpdateMode)
-            {
-                case PositionUpdateMode.None:
-                    break;
-                case PositionUpdateMode.Centroid:
-                    transform.Translation = centroid;
-                    break;
-                case PositionUpdateMode.BoxCenter:
-                    transform.Translation = (minBounds + maxBounds) / 2;
-                    break;
-            }
-        }
-
-        public void kill()
-        {
-            if (killDelayTime == 0)
-            {
-                //Destroy(gameObject);
-                return;
-            }
-
-            //points = new Vector3[0];
-            //StartCoroutine(killForReal(killDelayTime));
-        }
-
-        //IEnumerator killForReal(float timeBeforeKill)
+        //// Update is called once per frame
+        //internal void Update(float time)
         //{
-        //    yield return new WaitForSeconds(timeBeforeKill);
-        //    Destroy(gameObject);
+        //    if (time - lastUpdateTime > .5f) 
+        //        timeSinceGhost = time;
+        //    else 
+        //        timeSinceGhost = -1;
         //}
 
-        //void OnDrawGizmos() 
-        //{ 
-        //    if (drawDebug)
+        //public void updateData(float time, ReadOnlySpan<byte> data, int offset)
+        //{
+        //    var pos = offset + 1 + 2 * sizeof(int); //packet type (1) + packet size (4) + objectID (4)
+        //    while (pos < data.Length)
         //    {
-        //        Color c = Color.HSVToRGB((objectID * .1f) % 1, 1, 1); //Color.red;// getColor();
-        //        if (state == State.Ghost) c = Color.gray / 2;
+        //        var propertyID = Utils.ReadInt(data, pos);
+        //        var propertySize = Utils.ReadInt(data, pos + sizeof(int));
 
-        //        Gizmos.color = c;
-        //        foreach (var p in points) Gizmos.DrawLine(p, p + Vector3.forward * .01f);
+        //        if (propertySize < 0)
+        //        {
+        //            //Debug.LogWarning("Error : property size < 0");
+        //            break;
+        //        }
 
-        //        Gizmos.color = c + Color.white * .3f;
-        //        Gizmos.DrawWireSphere(centroid, .03f);
-        //        Gizmos.DrawWireCube((minBounds + maxBounds) / 2, maxBounds - minBounds);
+        //        switch (propertyID)
+        //        {
+        //            case 0: updatePointsData(data, pos + 2 * sizeof(int)); break;
+        //            case 1: updateClusterData(data, pos + 2 * sizeof(int)); break;
+        //        }
+
+        //        pos += propertySize;
+        //    }
+
+        //    lastUpdateTime = time;
+        //}
+
+        //void updatePointsData(ReadOnlySpan<byte> data, int offset)
+        //{
+        //    pointCount = Utils.ReadInt(data, offset);
+        //    var vectors = Utils.ReadVectors(data, offset + sizeof(int), pointCount * Unsafe.SizeOf<Vector3>());
+
+        //    if (pointsA.Length < pointCount)
+        //        pointsA = new Vector3[(int)(pointCount * 1.5)];
+
+        //    if (pointMode == CoordMode.Absolute)
+        //        vectors.CopyTo(pointsA.AsSpan());
+        //    else
+        //    {
+        //        for (int i = 0; i < vectors.Length; i++)
+        //            pointsA[i] = Vector3.Transform(vectors[i], parentTransform);
         //    }
         //}
+
+        //void updateClusterData(ReadOnlySpan<byte> data, int offset)
+        //{
+        //    state = (State)Utils.ReadInt(data, offset);
+        //    if (state == State.Leave) //Will leave
+        //    {
+        //        onRemove(this);
+        //        return;
+        //    }
+
+        //    var clusterData = new Vector3[4];
+        //    for (int i = 0; i < 4; i++)
+        //    {
+        //        var si = offset + sizeof(int) + i * Unsafe.SizeOf<Vector3>();
+
+        //        var p = Utils.ReadVector(data, si);
+        //        if (pointMode == CoordMode.Absolute || i == 1) //i=1 -> don't transform the velocity 
+        //            clusterData[i] = p; 
+        //        else
+        //            clusterData[i] = Vector3.Transform(p, parentTransform);//  parentTransform.InverseTransformPoint(p);
+        //    }
+
+        //    centroid = clusterData[0];
+        //    velocity = clusterData[1];
+        //    minBounds = clusterData[2];
+        //    maxBounds = clusterData[3];
+        //    weight = Utils.ReadFloat(data, offset + 4 + 4 * Unsafe.SizeOf<Vector3>());
+
+        //    switch (posUpdateMode)
+        //    {
+        //        case PositionUpdateMode.None:
+        //            break;
+        //        case PositionUpdateMode.Centroid:
+        //            transform.Translation = centroid;
+        //            break;
+        //        case PositionUpdateMode.BoxCenter:
+        //            transform.Translation = (minBounds + maxBounds) / 2;
+        //            break;
+        //    }
+        //}
+
+        protected override void UpdateTransform()
+        {
+            //throw new NotImplementedException();
+        }
     }
 }
